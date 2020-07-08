0f9b70c4393607492d6e40911c096f2b31e40966
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-07-08 18:38:31 +0300

    Create templates

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<h4>Hello world</h4>')
+    return render(request, 'blog/index.html')
 
 def about(request):
-    return HttpResponse('<h4>About us</h4>')
+    return render(request, '')
