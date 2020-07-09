2452d663ee1248ae86e7e7d762090bb20f191597
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-07-09 17:20:13 +0300

    Update tests.py

--- mysite/blog/tests.py
+++ mysite/blog/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import Task
 
-# Create your tests here.
+class TaskModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Task.objects.create(
+            title='first task',
+            task='make test on GitHub CI',
+        )
+
+    def test_title_content(self):
+        task = Task.objects.get(id=1)
+        expected_object_name = f'{task.title}'
+        self.assertEquals(expected_object_name, 'first task')
+
+    def test_description_content(self):
+        task = Task.objects.get(id=1)
+        expected_object_name = f'{task.task}'
+        self.assertEquals(expected_object_name, 'make test on GitHub CI')
